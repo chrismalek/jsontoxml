7090ae362f1732bd883ff8527231e94d85387e3c
Chris Malek <chris.r.malek@gmail.com>
2014-07-30 15:43:33 -0700

    more unit test work

--- web_test.go
+++ web_test.go
@@ -9,6 +9,22 @@ import (
 	"testing"
 )
 
+var jsonTestCases = []struct {
+	in  []byte
+	out string
+}{
+	{
+		in:  []byte(`{"root_element":{"foo":"bar1"}}`),
+		out: "<root_element><foo>bar1</foo></root_element>",
+	},
+	{
+		in:  []byte("{}"),
+		out: "<doc/>",
+	},
+}
+
+// {`{"root_element":{"foo":"bar1"}}`,"<root_element><foo>bar1</foo></root_element>" }}
+
 func TestHome(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(RootHandler))
 	defer ts.Close()
@@ -27,7 +43,7 @@ func TestPostJSON_1(t *testing.T) {
 
 	var jsondata = []byte(`{"root_element":{"foo":"bar1"}}`)
 
-	request, _ := http.NewRequest("POST", "/json2", bytes.NewReader(jsondata))
+	request, _ := http.NewRequest("POST", "/json2xml", bytes.NewReader(jsondata))
 
 	request.Header.Add("Content-Type", "application/json")
 
@@ -40,12 +56,37 @@ func TestPostJSON_1(t *testing.T) {
 		t.Errorf("Response body did not contain expected %v:\n\tbody: %v", "200", response.Code)
 	}
 
-	if !strings.Contains(body, "xxx<root_element><foo>bar1</foo></root_element>") {
+	if !strings.Contains(body, "<root_element><foo>bar1</foo></root_element>") {
 		t.Errorf("Response body did not contain expected %v:\n\t", body)
 	}
 
 }
 
-func TestJson2xml_1(t *testing.T) {
+func TestJson2xml_2(t *testing.T) {
+
+	for _, test := range jsonTestCases {
+
+		request, _ := http.NewRequest("POST", "/json2xml", bytes.NewReader(test.in))
+
+		request.Header.Add("Content-Type", "application/json")
+
+		response := httptest.NewRecorder()
+
+		json2Xml(response, request)
+
+		body := response.Body.String()
+
+		if response.Code != http.StatusOK {
+			t.Errorf("Response body did not contain expected %v:\n\tbody: %v", "200", response.Code)
+		}
+
+		if !strings.Contains(body, test.out) {
+			t.Errorf("Response body did not contain expected %v:\n\t", body)
+		}
+
+		// actual := Translate(test.in)
+		// assert.Equal(t, test.out, actual)
+	}
+
 	// t.Errorf("failed dude")
 }
